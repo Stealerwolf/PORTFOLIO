ff7a0fb8d3641ace656368d8989fc98681418ef3
stealerwolf <davimuza90@gmail.com>
2024-01-28 23:49:12 -0300

    added a card with a header

--- portfolio/src/components/Cards.tsx
+++ portfolio/src/components/Cards.tsx
@@ -1,7 +1,21 @@
 function Cards() {
   return (
-    <div className="container-xxl">
-      100% wide until extra extra large breakpoint
+    <div className="card text-bg-dark mb-3  " style={{ maxWidth: "18rem" }}>
+      <div className="card-header">Header</div>
+      <div className="card-body">
+        <h5 className="card-title">Dark card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">Dark card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
     </div>
   );
 }
