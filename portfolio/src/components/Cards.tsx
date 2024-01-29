3509ee781417d104c7838f983c8b93cb3a25dc1f
stealerwolf <davimuza90@gmail.com>
2024-01-29 12:02:06 -0300

    Addition of img folder, fixed cards, added emoji

--- portfolio/src/components/Cards.tsx
+++ portfolio/src/components/Cards.tsx
@@ -1,20 +1,38 @@
+import Assunto from "../Images/Assunto.png";
+
 function Cards() {
   return (
-    <div className="card text-bg-dark mb-3  " style={{ maxWidth: "18rem" }}>
-      <div className="card-header">Header</div>
-      <div className="card-body">
-        <h5 className="card-title">Dark card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+      <div className="card text-bg-dark mb-3  " style={{ maxWidth: "18rem" }}>
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Dark card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
       </div>
-      <div className="card-body">
-        <h5 className="card-title">Dark card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+      <div className="card text-bg-dark mb-3  " style={{ maxWidth: "18rem" }}>
+        <img src={Assunto} className="card-img-top" alt="yeah" />
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Dark card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <div className="card text-bg-dark mb-3 " style={{ maxWidth: "18rem" }}>
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Dark card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
       </div>
     </div>
   );
