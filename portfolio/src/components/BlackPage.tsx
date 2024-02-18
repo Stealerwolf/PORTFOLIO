1aa2e2d15d6d5d57365e4b9792d0ffd78d5dd4e3
stealerwolf <davimuza90@gmail.com>
2024-02-18 05:29:06 -0300

    text aligning and more cosmetic changes

--- portfolio/src/components/BlackPage.tsx
+++ portfolio/src/components/BlackPage.tsx
@@ -1,9 +1,10 @@
 import Text from "./Text";
+import BlackPageText from "./BlackPageText";
 
 function BlackPage() {
   return (
     <div className="BlackPage">
-      <Text />
+      <BlackPageText />
     </div>
   );
 }
---
+++ portfolio/src/components/BlackPageText.tsx
@@ -0,0 +1,11 @@
+function BlackPageText() {
+  return (
+    <div className="BlackPageText">
+      EXPLORE MORE
+      <br /> PROJECTS BY
+      <br /> CLICKING HERE
+    </div>
+  );
+}
+
+export default BlackPageText;
