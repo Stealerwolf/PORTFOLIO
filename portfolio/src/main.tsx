a7e1b6f93e950cba71ac862f616fd9742dd99a63
stealerwolf <davimuza90@gmail.com>
2024-03-03 23:48:13 -0300

    letter

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -8,7 +8,7 @@ import Offcanvas from "./components/Offcanvas.tsx";
 import Text from "./components/Text.tsx";
 import Cards from "./components/Cards.tsx";
 import BlackPage from "./components/BlackPage.tsx";
-import Carousel from "./components/carousel.tsx";
+import Carousel from "./components/Carousel.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
